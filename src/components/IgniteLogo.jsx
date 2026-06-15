1f8368d1d25e1fc618d592f77d87a054df9767bb
Carlos Miranda <carlos.miranda@ivcevidensia.com>
2026-06-15 10:49:46 +0100

    Fix GitHub Pages logo path and update docs build

--- src/components/IgniteLogo.jsx
+++ src/components/IgniteLogo.jsx
@@ -2,7 +2,7 @@ export default function IgniteLogo({ size = 48, style = {}, align = 'center' }) 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: align, ...style }}>
       <img
-        src="/ignite.png"
+        src="ignite.png"
         alt="Ignite Individual Coaching"
         style={{ width: size * 4, height: 'auto', display: 'block' }}
       />
